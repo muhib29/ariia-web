7b595f0a94f3d9aa85ebf1b14ea8a68e49b40abb
muhib29 <syedmuhibfarooq@gmail.com>
2026-05-25 01:17:15 +0500

    issue 4

--- apps/web/original_Zip_file/web/config/legal-dates.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Effective date for Terms of Service and Privacy Policy.
- * Update this single value when you publish new versions of the ToS or PP.
- */
-export const LEGAL_EFFECTIVE_DATE = 'April 1, 2026';
